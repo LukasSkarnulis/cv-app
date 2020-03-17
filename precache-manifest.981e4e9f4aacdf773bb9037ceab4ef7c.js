2746ffc900d328c5dc6ec696607e138b7483e9ff
LuksiusLukas2 <luksiuslukas2@gmail.com>
2020-03-17 15:50:07 +0200

    Updates

---
+++ precache-manifest.981e4e9f4aacdf773bb9037ceab4ef7c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741dd20f88b9ab5887a881dcdbd73b14",
+    "url": "/cv-app/index.html"
+  },
+  {
+    "revision": "fd36dca33a41ef305d88",
+    "url": "/cv-app/static/css/main.8669a0c2.chunk.css"
+  },
+  {
+    "revision": "d5b507e1459f9b45754d",
+    "url": "/cv-app/static/js/2.bf8a5403.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/cv-app/static/js/2.bf8a5403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd36dca33a41ef305d88",
+    "url": "/cv-app/static/js/main.6553cf63.chunk.js"
+  },
+  {
+    "revision": "b18e1a249002ad9bd749",
+    "url": "/cv-app/static/js/runtime-main.cc4cd89b.js"
+  },
+  {
+    "revision": "22460487507c9e8f3560952c5423ce44",
+    "url": "/cv-app/static/media/Image1.22460487.jpg"
+  },
+  {
+    "revision": "3481ba46a8e4b111843576f032577319",
+    "url": "/cv-app/static/media/Image2.3481ba46.jpg"
+  },
+  {
+    "revision": "e2dfea97af67e3d5979639e243eb8979",
+    "url": "/cv-app/static/media/cv-picture.e2dfea97.png"
+  },
+  {
+    "revision": "5c112f27131325c32cc01a32ca721fe5",
+    "url": "/cv-app/static/media/white geometric background 0302.5c112f27.jpg"
+  }
+]);
