ea827b3b34549e7b50526cfaf00c4098ae385c39
LuksiusLukas2 <luksiuslukas2@gmail.com>
2020-03-21 11:53:09 +0200

    Additional changes

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -6,14 +6,9 @@ class AboutMe extends React.Component {
         return (
             <div className="aboutMe">
             <h1>Hello,</h1><br />
-            <p>My name is Lukas, I am 24 years old and I have recently started learning web 
-                development. I have tried a few different activities, but due to either personal 
-                disinterest or lack of career opportunities, none of them have 
-                stuck with me for long. And since the IT business is always growing and I 
-                have heard several success stories in there, I have decided to hopefully 
-                start a new and successful career path in web development myself.</p><br /><br /><br /><br /><br />
-            <p>Since the start of February I have begun independent lessons at Codecademy
-                as front-end web developer. Since that time I have improved my knowledge 
+            <p>I am a self taught beginner front-end web developer, who is looking for 
+                work experience. Since the start of February I have begun independent lessons
+                at Codecademy. As a result, I have improved my knowledge 
                 in HTML, CSS and learned JavaScript, GitHub and React. To show off what 
                 I have learned, I made this CV-website from scratch with React, where I 
                 share my work experience, skills and projects.</p><br /><br /><br /><br /><br />
@@ -23,8 +18,7 @@ class AboutMe extends React.Component {
                     I am looking for an internship, where I could start building a solid 
                     career path at web development. I am eager to learn, improve and gain 
                     invaluable work experience. I thank you for your time, for reading 
-                    this and I eagerly await your answer. I also understand that this is 
-                    not a good time to look for internship, but I prefer to try at least.</p><br /><br /><br /><br /><br />   
+                    this and I eagerly await your answer.</p><br /><br /><br /><br /><br />   
             </div>
         )
     }
